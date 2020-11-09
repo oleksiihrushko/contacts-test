7dd930d01f0e0c78ddce83a76e9048368f362372
hrushkooleksii <oleksiihrushko@gmail.com>
2020-11-09 11:35:11 +0200

    Updates

---
+++ precache-manifest.4d4323812f2f8a5a36e7e038f3de1d75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c8b281fcdd2da7982e793ef18ba5a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "396969c499ac3f2f9e58",
+    "url": "/static/css/2.5c600ed6.chunk.css"
+  },
+  {
+    "revision": "6908f8f7fab8e6143129",
+    "url": "/static/css/main.fc51070f.chunk.css"
+  },
+  {
+    "revision": "396969c499ac3f2f9e58",
+    "url": "/static/js/2.67cbfe8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.67cbfe8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6908f8f7fab8e6143129",
+    "url": "/static/js/main.b6c89f6f.chunk.js"
+  },
+  {
+    "revision": "7123c836dd9e39bc8248",
+    "url": "/static/js/runtime-main.051cbb2f.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
